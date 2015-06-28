29738aab098cff3eef70465c2ca15fd20cd7ef17
Nicole Daniels <ndaniels320gmail.com>
2015-06-28 18:09:45 -0400

    Implement ladda add cash button on refill page

--- app/scripts/controllers/refill.js
+++ app/scripts/controllers/refill.js
@@ -8,14 +8,18 @@
  * Controller of the laundrytrackerApp
  */
 angular.module('laundrytrackerApp')
-  .controller('RefillCtrl', function ($scope, localStorageService, $location) {
+  .controller('RefillCtrl', function ($scope, $timeout, localStorageService, $location) {
     var laundryBalance = localStorageService.get('balance');
     $scope.balance = laundryBalance || 0;
      
     $scope.addCash = function() {
-    	$scope.balance += $scope.depositAmount;
-    	$location.path('#/');
-		};
+    	$scope.loading = true;
+    	$timeout(function () {
+    		$scope.loading = false; // stop loading
+    		$scope.balance += $scope.depositAmount;
+    		$location.path('#/');
+    	}, 500);
+    };
 
     $scope.$watch('balance', function () {
     	localStorageService.set('balance',$scope.balance);
