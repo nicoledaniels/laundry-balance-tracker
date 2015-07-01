1f51772aecf47d51d789374de2ad64e9463eaa99
Nicole Daniels <ndaniels320gmail.com>
2015-06-30 20:33:02 -0400

    Change navigation ordering an erase ngTable pagination

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -11,20 +11,5 @@ angular.module('laundrytrackerApp')
   .controller('ReportsCtrl', function ($scope, $firebaseArray, ngTableParams) {
     var ref = new Firebase("https://laundrytracker.firebaseio.com/");
     $scope.laundryData= $firebaseArray(ref);
-    //console.log($scope.laundryData.length);
-
-    $scope.laundryData.$loaded().then(function(laundryData) {
-   		console.log(laundryData.length); // data is loaded here
-   		$scope.arrayLength = laundryData.length;
 	});
-
-    $scope.tableParams = new ngTableParams({
-        page: 1,            // show first page
-        count: 10           // count per page
-    }, {
-        total: $scope.arrayLength, // length of data
-        getData: function ($defer, params) {
-            $defer.resolve($scope.laundryData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-        }
-    })
   });
